c9a20769426f205699276f9483e9f870222d8a7d
PakaOxO <arpeggiogio@gmail.com>
2024-03-28 11:39:53 +0900

    [Feat] 인기 조회 캐릭터 api 작성

--- portal-client/app/ui/maple/BottomContainer.tsx
+++ portal-client/app/ui/maple/BottomContainer.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import FamousCharacter from "./FamousCharacter";
 
 const BottomContainer = () => {
-  return <div className="w-full h-2/5 relative flex justify-center box-border"></div>;
+  return (
+    <div className="w-full h-2/5 relative flex justify-center box-border">
+      <FamousCharacter />
+      <FamousCharacter />
+    </div>
+  );
 };
 
 export default BottomContainer;
---
+++ portal-client/app/ui/maple/FamousCharacter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const FamousCharacter = () => {
+  return <div className="w-[360px] h-[240px] bg-slate-50 mx-[16px]"></div>;
+};
+
+export default FamousCharacter;
+
